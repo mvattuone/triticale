dfbb76ce1f814842a2ad1f78cd29dc2ed15cb8b5
Michael Vattuone <mvattuone@gmail.com>
2024-04-25 00:09:40 -0400

    Add some window functions

---
+++ synth-select.js
@@ -0,0 +1,44 @@
+export default class SynthSelect extends HTMLElement {
+  constructor() {
+    super();
+
+    const selectName = this.getAttribute('name');
+    const options = ['blackman', 'gaussian', 'hann', 'hamming', 'triangle', 'none'];
+
+    this.attachShadow({ mode: "open" });
+
+
+    this.shadowRoot.innerHTML = `
+      <select name="${selectName}">
+        ${options.map(option => `<option value=${option}>${option}</option>`)}
+      </select>
+    `;
+
+    this.selectElement = this.shadowRoot.querySelector('select');
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+
+  handleOnChange(e) {
+    const { name, value } = e.target;
+
+
+    const updateConfigEvent = new CustomEvent("update-config", {
+      detail: { name, value },
+      bubbles: true,
+      composed: true,
+    });
+
+    document
+      .querySelector("synth-brain")
+      .dispatchEvent(updateConfigEvent);
+  }
+
+  connectedCallback() {
+    this.selectElement.addEventListener('change', this.handleOnChange);
+  }
+
+  disconnectedCallback() {}
+}
+
+customElements.define("synth-select", SynthSelect);
+
