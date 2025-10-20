d76210f04940ffe682a54406e2f2ce8d75f9c04b
Michael Vattuone <mvattuone@gmail.com>
2025-10-20 14:19:33 -0400

    Update importmap to take into account github pages usage

--- helpers/pizzicato.js
+++
@@ -1,9 +0,0 @@
-import "/node_modules/pizzicato/distr/Pizzicato.min.js";
-
-const { Pizzicato } = window;
-
-if (!Pizzicato) {
-  throw new Error("Pizzicato failed to load from /node_modules/pizzicato/distr/Pizzicato.min.js");
-}
-
-export default Pizzicato;
