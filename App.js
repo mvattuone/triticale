88d9c0232c109de198aebe894b6aca6e8d8a9e01
Michael Vattuone <mvattuone@gmail.com>
2025-11-03 23:42:24 -0500

    Remove cruft

--- App.js
+++ App.js
@@ -3,8 +3,6 @@ import "./synth-brain.js";
 import "./synth-display.js";
 import "./synth-select.js";
 import "./synth-dial.js";
-import "./synth-slider.js";
-import "./synth-toggle.js";
 import "./synth-switch.js";
 import "./synth-waveform.js";
 import "./latch-button.js";
--- synth-toggle.js
+++
@@ -1,7 +0,0 @@
-import SynthSwitch from "./synth-switch.js";
-
-export default class SynthToggle extends SynthSwitch {}
-
-if (!customElements.get("synth-toggle")) {
-  customElements.define("synth-toggle", SynthToggle);
-}
